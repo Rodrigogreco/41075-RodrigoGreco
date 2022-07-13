e0e0e95fe6c7aeff96e4175327866feb84363a61
Rodrigo Greco <rodrigreco10@gmail.com>
2022-07-13 20:28:48 -0300

    Creando Algoritmo con ciclo

---
+++ js/main.js
@@ -0,0 +1,8 @@
+for (let i=1; i<10;i++){
+    let ingresarNombre = prompt('Ingresar Nombre')
+    if(ingresarNombre === 'ESC'){
+        break;
+    }
+    alert("Turno N° " +i+ "Nombre:"+ingresarNombre)
+}
+
